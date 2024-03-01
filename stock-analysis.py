ba0f0714763338b5b46ec43be2423842a9e27594
junting <10355098@st.thsh.tp.edu.tw>
2024-03-02 01:10:02 +0800

    Refactor buy and sell conditions in stock-analysis.py

--- stock-analysis.py
+++ stock-analysis.py
@@ -16,7 +16,7 @@ price_break_high_3m = close >= high_3m
 # 總市值在150億台幣以上
 # market_value_condition = market_value > 15000000000  # 150億台幣
 
-# 綜合買入條件
+# 買入條件
 buy_condition = (
     above_ma60 &
     ma60_rising &
@@ -24,8 +24,22 @@ buy_condition = (
     # market_value_condition
 )
 
+below_ma60 = close < ma60
+not_recover_in_5_days = below_ma60.sustain(5)
+ma60_falling = ma60 < ma60.shift(1)
+# 計算每天股票價格相比前一天收盤價的變動百分比
+price_change_percent = close.pct_change()
+# 設定停板條件，即價格跌幅小於或等於-10%
+hit_drop_limit = price_change_percent <= -0.10
+
+
+# 賣出條件
+sell_condition = ( not_recover_in_5_days | ma60_falling | hit_drop_limit )
+
+position = buy_condition.hold_until(sell_condition)
+
 # 使用 sim 函數進行模擬
-report = backtest.sim(buy_condition, resample='D', name="吳Peter策略選股")
+report = backtest.sim(position, resample=None, name="吳Peter策略選股")
 
 
 if __name__ == "__main__":

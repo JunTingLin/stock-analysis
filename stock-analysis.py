057c506a380cf131cc3b623229690d285553d83b
junting <10355098@st.thsh.tp.edu.tw>
2024-03-07 23:46:53 +0800

    Add output and *.zip to .gitignore, and update stock-analysis.py with EPS condition

--- stock-analysis.py
+++ stock-analysis.py
@@ -6,6 +6,7 @@ from get_data import get_data_from_finlab
 close = get_data_from_finlab("price:收盤價", use_cache=True, market='TSE_OTC')
 market_value = get_data_from_finlab("etl:market_value", use_cache=True, market='TSE_OTC')
 market_value = market_value[:'2020-12-31 00:00:00']
+eps = get_data_from_finlab('financial_statement:每股盈餘', use_cache=True, market='TSE_OTC')
 
 # with data.universe(market='TSE_OTC'):
 #     close = data.get('price:收盤價')
@@ -21,13 +22,19 @@ high_3m = close.rolling(60).max()
 price_break_high_3m = close >= high_3m
 # 總市值在150億台幣以上
 market_value_condition = market_value > 15000000000  # 150億台幣
+# 過去四個季度的盈餘總和大於2元，且連續兩年都滿足這個條件
+cumulative_eps_last_year = eps.rolling(4).sum() > 2
+cumulative_eps_year_before_last = eps.shift(4).rolling(4).sum() > 2
+eps_condition = cumulative_eps_last_year & cumulative_eps_year_before_last
+
 
 # 買入條件
 buy_condition = (
     above_ma60 &
     ma60_rising &
     price_break_high_3m &
-    market_value_condition
+    market_value_condition &
+    eps_condition
 )
 
 below_ma60 = close < ma60

589d71afa242c8d5d4fc428a7cb1ddbffc46d457
junting <10355098@st.thsh.tp.edu.tw>
2024-03-12 00:14:13 +0800

    Add revenue growth condition to buy condition

--- stock-analysis.py
+++ stock-analysis.py
@@ -2,11 +2,12 @@ import pandas as pd
 from finlab import data
 from finlab import backtest
 from get_data import get_data_from_finlab
-from utils import calculate_rolling_slope
 
 close = get_data_from_finlab("price:收盤價", use_cache=True, market='TSE_OTC')
 market_value = get_data_from_finlab("etl:market_value", use_cache=True, market='TSE_OTC')
 eps = get_data_from_finlab('financial_statement:每股盈餘', use_cache=True, market='TSE_OTC')
+# 取得去年同月營業額增減百分比
+revenue_growth_yoy = get_data_from_finlab('monthly_revenue:去年同月增減(%)', use_cache=True, market='TSE_OTC')
 
 # with data.universe(market='TSE_OTC'):
 #     close = data.get('price:收盤價')
@@ -14,11 +15,10 @@ eps = get_data_from_finlab('financial_statement:每股盈餘', use_cache=True, m
 
 # 計算季線（60日移動平均）並判斷季線是否上升
 ma60 = close.average(60)
-window = 60
-ma60_slope = close.apply(lambda x: calculate_rolling_slope(x, window), axis=0)
-ma60_rising = ma60_slope > 0
+# 使用rise函數檢查ma60是否在過去nwindow天連續上升
+ma60_rising = ma60.rise(1)
 
-# # 股價是否大於季線
+# 股價是否大於季線
 above_ma60 = close > ma60
 # 股價突破三個月高點
 high_3m = close.rolling(60).max()
@@ -40,6 +40,9 @@ cumulative_eps_last_year = eps.rolling(4).sum() > 2
 cumulative_eps_year_before_last = eps.shift(4).rolling(4).sum() > 2
 eps_condition = cumulative_eps_last_year & cumulative_eps_year_before_last
 
+# 設定營業額成長的條件，例如選擇佔比大於10%的股票
+revenue_growth_condition = revenue_growth_yoy > 10
+
 
 # 買入條件
 buy_condition = (
@@ -47,7 +50,8 @@ buy_condition = (
     ma60_rising &
     price_break_high_3m &
     market_value_condition_expanded &
-    eps_condition
+    eps_condition &
+    revenue_growth_condition
 )
 # 刪除最後一row
 buy_condition = buy_condition[:-1]

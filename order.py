7a37f162de7657d4398b20c6ac15aa6e18848091
junting <10355098@st.thsh.tp.edu.tw>
2024-04-30 03:17:05 +0800

    Selectively push important information to Line

--- order.py
+++ order.py
@@ -8,6 +8,7 @@ from decimal import Decimal
 import logger_config
 import logging
 
+send_msg = []
 
 # 設置日誌
 logger_config.setup_logging()
@@ -28,10 +29,17 @@ acc = FugleAccount()
 
 # 執行交易邏輯
 fund = 100000 * 0.8  # 設置資金為現金的80%
-logging.info(f"當前帳戶現金為{acc.get_cash()}")
+logging.info(f"1. 當前帳戶目前現金為: NT${acc.get_cash()}")
+send_msg.append(f"1. 當前帳戶目前現金為: NT${acc.get_cash()}")
 
 acc_position = acc.get_position()
-logging.info(f"當前帳戶目前持倉為{acc_position.position}")
+logging.info(f"2. 當前帳戶目前持倉為: {acc_position.position}")
+send_msg.append(f"2. 當前帳戶目前持倉為: ")
+for position in acc_position.position:
+    stock_id = position['stock_id']
+    quantity = position['quantity']
+    send_msg += f"{stock_id} : {quantity} 張\n"  # normalize() 移除小數點後多餘的零
+
 current_ids = set(p['stock_id'] for p in acc_position.position)
 
 # 獲取由report物件生成的今日持倉狀態
@@ -48,16 +56,28 @@ month_trading_days = close.loc[month_start:today]
 if not acc_position.position:
     order_executor = OrderExecutor(position_today, account=acc)
     order_executor.create_orders()  # 調整到這個持倉
-    logging.info(f"今天是{today}，帳戶股票部位都是空的狀況，初始化持倉")
-    logging.info(f"將於下一個交易調整持倉為{position_today.position}")
+    logging.info(f"今天是{today.date()}，帳戶股票部位都是空的狀況，初始化持倉")
+    send_msg.append(f"今天是{today.date()}，帳戶股票部位都是空的狀況，初始化持倉")
+    logging.info(f"將於下一個交易調整持倉為: {position_today.position}")
+    send_msg.append(f"將於下一個交易調整持倉為: ")
+    for position in position_today.position:
+        stock_id = position['stock_id']
+        quantity = position['quantity']
+        send_msg.append(f"{stock_id} : {quantity} 張\n")
 
 
 if not month_trading_days.empty:
     first_trading_day = month_trading_days.index[0]
     last_trading_day = month_trading_days.index[-1]
     if today == first_trading_day:
-        logging.info(f"今天是{today}，為該月實際的第一個交易日，將執行換股調整")
+        logging.info(f"今天是{today.date()}，為該月實際的第一個交易日，將執行換股調整")
+        send_msg.append(f"今天是{today.date()}，為該月實際的第一個交易日，將執行換股調整")
         logging.info(f"將於下一個交易調整持倉為{position_today.position}")
+        send_msg.append(f"將於下一個交易調整持倉為: ")
+        for position in position_today.position:
+            stock_id = position['stock_id']
+            quantity = position['quantity']
+            send_msg.append(f"{stock_id} : {quantity} 張\n")
         # 如果今天是該月實際的第一個交易日，則執行換股調整
         position_today = Position.from_report(report, fund, odd_lot=True)
         order_executor = OrderExecutor(position_today, account=acc)
@@ -67,7 +87,8 @@ if not month_trading_days.empty:
         # 如果今天和close的最後一個交易日相同，則判斷帳戶股票部位是否有要提前出售的(跌8%)
         remove_ids = current_ids - new_ids
         if remove_ids:
-            logging.info(f"今天是{today}，需要移除的股票有{remove_ids}，將於下一個交易日出售")
+            logging.info(f"今天是{today.date()}，需要移除的股票有{remove_ids}，將於下一個交易日出售")
+            send_msg.append(f"今天是{today.date()}，需要移除的股票有{remove_ids}，將於下一個交易日出售")
             # 為需要移除的股票設置數量為0，其它幾檔的數量維持不變
             for position in acc_position.position:
                 if position['stock_id'] in remove_ids:
@@ -76,15 +97,19 @@ if not month_trading_days.empty:
             order_executor = OrderExecutor(acc_position, account=acc)
             order_executor.create_orders()
         else:
-            logging.info(f"今天是{today}，沒有跌停的股票需要出售，不需要調整持倉")
+            logging.info(f"今天是{today.date()}，沒有跌停的股票需要出售，不需要調整持倉")
+            send_msg.append(f"今天是{today.date()}，沒有跌停的股票需要出售，不需要調整持倉")
     else:
-        logging.info(f"今天{today}是休市日")
+        logging.info(f"今天{today.date()}是休市日")
+        send_msg.append(f"今天{today.date()}是休市日")
 
 else:
-    logging.info(f"今天{today}是休市")
+    logging.info(f"今天{today.date()}是休市")
+    send_msg.append(f"今天{today.date()}是休市")
 
-# 發送日誌內容到LINE
-logger_config.send_log_contents()
+# 發送容到LINE
+from line_bot_notifier import send_line_notification
+send_line_notification("\n".join(send_msg))
 
 
     

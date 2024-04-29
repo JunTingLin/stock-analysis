a36b1e58229daa8b996bef16d76fb96fcee70df0
junting <10355098@st.thsh.tp.edu.tw>
2024-04-29 22:54:13 +0800

    feat: Order logic

--- TibetanMastiff_TW_Strategy.py
+++ TibetanMastiff_TW_Strategy.py
@@ -40,6 +40,9 @@ report = sim(buy , resample="M", upload=False, position_limit=1/3, fee_ratio=1.4
 from finlab.online.fugle_account import FugleAccount
 import os
 import configparser
+import pandas as pd
+from decimal import Decimal
+
 config = configparser.ConfigParser()
 config_file_name = 'config.simulation.ini'
 config.read(config_file_name)
@@ -48,34 +51,70 @@ os.environ['FUGLE_CONFIG_PATH'] = config_file_name
 os.environ['FUGLE_MARKET_API_KEY'] = config['FUGLE_MARKET']['FUGLE_MARKET_API_KEY']
 acc = FugleAccount()
 
-from finlab.online.order_executor import Position
-# total fund
-fund = 10000
-position_today = Position.from_report(report, fund, odd_lot=True)
-print(position_today)
+from finlab.online.order_executor import Position, OrderExecutor
+# 每次策略資金都是當前帳戶的現金80%
+fund = acc.get_cash() * 0.8
+print(f"當前帳戶現金為{acc.get_cash()}")
 
-from finlab.online.order_executor import OrderExecutor
-order_executor = OrderExecutor(position_today, account=acc)
-# 新增委託單
-order_executor.create_orders()
+# 獲取目前帳戶的持倉狀態
+acc_position = acc.get_position()
+print(f"當前帳戶持倉為{acc_position.position}")
+current_ids = set(p['stock_id'] for p in acc_position.position)
+
+# 獲取由report物件生成的今日持倉狀態
+position_today = Position.from_report(report, fund, odd_lot=True)
+new_ids = set(p['stock_id'] for p in position_today.position)
+
+# 檢查是否acc_position持倉陣列是空的，例如初始狀態，帳戶股票部位都是空的狀況
+if not acc_position.position:
+    order_executor = OrderExecutor(position_today, account=acc)
+    order_executor.create_orders()  # 調整到這個持倉
+
+
+# 檢查今天是否為月初第一天，月初第一天確定換股調整
+today = pd.Timestamp.now().normalize()  # 正規化以去除時間部分
+# 判斷今天是否為月份至今的最後一個交易日
+month_start = today.replace(day=1)
+month_trading_days = close.loc[month_start:today].index
+
+if not month_trading_days.empty:
+    first_trading_day = month_trading_days.index[0]
+    last_trading_day = month_trading_days.index[-1]
+    if today == first_trading_day:
+        print(f"今天是{today}，為該月實際的第一個交易日，將執行換股調整，於下一個交易日生效")
+        # 如果今天是該月實際的第一個交易日，則執行換股調整
+        position_today = Position.from_report(report, fund, odd_lot=True)
+        order_executor = OrderExecutor(position_today, account=acc)
+        order_executor.create_orders()  # 調整到這個持倉
+
+    elif today == last_trading_day:
+        # 如果今天和close的最後一個交易日相同，則判斷帳戶股票部位是否有要提前出售的(跌8%)
+        remove_ids = current_ids - new_ids
+        if remove_ids:
+            print(f"今天是{today}，需要移除的股票有{remove_ids}，將於下一個交易日出售")
+            # 為需要移除的股票設置數量為0，其它幾檔的數量維持不變
+            for position in acc_position.position:
+                if position['stock_id'] in remove_ids:
+                    position['quantity'] = Decimal('0')
+
+            order_executor = OrderExecutor(acc_position, account=acc)
+            order_executor.create_orders()
+        else:
+            print(f"今天是{today}，沒有跌停的股票需要出售，不需要調整持倉")
+    else:
+        print(f"今天{today}是休市日")
+
+else:
+    print(f"今天{today}是休市")
+
+    
+
 
 # 刪除委託單
-order_executor.cancel_orders()
+# order_executor.cancel_orders()
 
 # 拿到當前帳戶的股票部位
-acc.get_position()
+# acc.get_position()
 
 
-# ----------------------------------------------------------------
 
-from report_analyzer import ReportAnalyzer
-# 分析報告
-analyzer = ReportAnalyzer(report)
-analysis_result = analyzer.analyze_trades_for_date('2024-03-01')
-print(analysis_result)
-
-from report_saver import ReportSaver
-# 保存報告和交易記錄
-saver = ReportSaver(report)
-saver.save_report_html()
-saver.save_trades_excel()

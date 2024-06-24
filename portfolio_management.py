7c32584f6dce702049604271c56341728d1bdd25
junting <junting0218@gmail.com>
2024-06-25 01:12:02 +0800

    Modify portfolio_details decimal places

--- portfolio_management.py
+++ portfolio_management.py
@@ -1,13 +1,14 @@
 def calculate_portfolio_value(positions, close_prices, company_basic_info):
     total_value = 0
     portfolio_details = []
+    
     for position in positions:
         stock_id = position['stock_id']
-        quantity = position['quantity']
+        quantity = float(position['quantity'])  # 將 quantity 統一轉型為 float
+
         if stock_id in close_prices:
-            close_price = close_prices[stock_id].iloc[-1]  # 取得最後一筆交易日的收盤價
-            stock_value = close_price * float(quantity) *1000  # 將 Decimal 轉換為 float 進行計算
-            stock_value = int(stock_value)
+            close_price = round(close_prices[stock_id].iloc[-1], 2)  # 取得最後一筆交易日的收盤價
+            stock_value = int(close_price * quantity * 1000)
             total_value += stock_value
 
             # 查找公司簡稱
@@ -17,7 +18,7 @@ def calculate_portfolio_value(positions, close_prices, company_basic_info):
             portfolio_details.append({
                 'stock_id': stock_id,
                 'stock_name': stock_name,
-                'quantity': float(quantity),
+                'quantity': quantity,
                 'close_price': close_price,
                 'stock_value': stock_value
             })

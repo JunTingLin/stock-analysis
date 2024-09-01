d57fab95a1922574fd82a0edcb287dc0559a88cf
junting <junting0218@gmail.com>
2024-09-01 16:27:45 +0800

    feat: Add port number to app.run in app.py and cancel_order.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 AutoIndex(app, browse_root='output')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
---
+++ cancel_order.py
@@ -0,0 +1,15 @@
+from authentication import Authenticator
+from config_loader import ConfigLoader
+from finlab.online.order_executor import OrderExecutor
+
+
+config_loader = ConfigLoader()
+config_loader.load_env_vars()
+
+auth = Authenticator()
+auth.login_finlab()
+acc = auth.login_fugle()
+
+order_executor = OrderExecutor(None, acc)
+order_executor.cancel_orders()
+print("cancel orders successfully")
